97d3dadd64cf3ae5a4984e2e271da937cd87ddb9
Y4h14 <yahyasadig2013@gmail.com>
2023-11-16 12:51:41 +0300

    memeroy fix

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,9 +1,6 @@
 push 5
-push 100
-push 25
-div
 push 50
 push 50
+push 0
 mod
-push 1
 pall
--- op_func3.c
+++ op_func3.c
@@ -15,6 +15,7 @@ int div_top(stack_t **top, unsigned int ln, char **inst)
 	if (stack_hight(*top) < 2)
 	{
 		fprintf(stderr, "L%d: can't div stack too short\n", ln);
+		free_mem(inst);
 		return (-1);
 	}
 
@@ -22,12 +23,14 @@ int div_top(stack_t **top, unsigned int ln, char **inst)
 	if (temp->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", ln);
+		free_mem(inst);
 		return (-1);
 	}
 
 	(temp->next)->n = (temp->next)->n / temp->n;
-	*top = temp->next;
+	/**top = temp->next;*/
 	free(temp);
+	pop_top(top, ln, inst);
 	return (1);
 }
 /**
@@ -46,6 +49,7 @@ int mod_top(stack_t **top, unsigned int ln, char **inst)
 	if (stack_hight(*top) < 2)
 	{
 		fprintf(stderr, "L%d: can't div stack too short\n", ln);
+		free_mem(inst);
 		return (-1);
 	}
 
@@ -53,11 +57,13 @@ int mod_top(stack_t **top, unsigned int ln, char **inst)
 	if (temp->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", ln);
+		free_mem(inst);
 		return (-1);
 	}
 
 	(temp->next)->n = (temp->next)->n % temp->n;
-	*top = temp->next;
+	/**top = temp->next;*/
 	free(temp);
+	pop_top(top, ln, inst);
 	return (1);
 }
